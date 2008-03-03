b900a09f46feb1f66e04f81ade06f5746d491b23
john.imageengine <john.imageengine@07b78fde-9e1a-0410-98de-79dcf2039bee>
2008-03-03 20:03:55 +0000

    Added a ScopedMessageHandler class to assist in the pushHandler()/popHandler() nesting necessary when using different MessageHandlers.

--- branches/imageRework/src/IECore/bindings/IECore.cpp
+++ branches/imageRework/src/IECore/bindings/IECore.cpp
@@ -154,6 +154,7 @@
 #include "IECore/bindings/ImageCropOpBinding.h"
 #include "IECore/bindings/MeshPrimitiveShrinkWrapOpBinding.h"
 #include "IECore/bindings/ImagePrimitiveEvaluatorBinding.h"
+#include "IECore/bindings/ScopedMessageHandlerBinding.h"
 #include "IECore/IECore.h"
 
 using namespace IECore;
@@ -287,6 +288,7 @@ BOOST_PYTHON_MODULE(_IECore)
 	bindImageCropOp();
 	bindMeshPrimitiveShrinkWrapOp();
 	bindImagePrimitiveEvaluator();
+	bindScopedMessageHandler();
 		
 	def( "majorVersion", &IECore::majorVersion );
 	def( "minorVersion", &IECore::minorVersion );
