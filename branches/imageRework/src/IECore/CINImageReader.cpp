8f700e293ad7042712469ba6ed15d189c571489e
mark.imageengine <mark.imageengine@07b78fde-9e1a-0410-98de-79dcf2039bee>
2008-03-10 23:46:06 +0000

    Reinstated Cineon reader, only with modifications required to conform to new ImageReader interface

--- branches/imageRework/src/IECore/CINImageReader.cpp
+++ branches/imageRework/src/IECore/CINImageReader.cpp
@@ -58,64 +58,46 @@ using namespace std;
 
 const Reader::ReaderDescription<CINImageReader> CINImageReader::m_readerDescription("cin");
 
-CINImageReader::CINImageReader() : 
-		ImageReader( "CINImageReader", "Reads Kodak Cineon (CIN) files." ),
-		m_buffer(0)
+CINImageReader::CINImageReader() :
+		ImageReader( "CINImageReader", "Reads Kodak Cineon (CIN) files." )
 {
 }
 
-CINImageReader::CINImageReader(const string & fileName) : 
-		ImageReader( "CINImageReader", "Reads Kodak Cineon (CIN) files." ),
-		m_buffer(0)
+CINImageReader::CINImageReader( const string &fileName ) :
+		ImageReader( "CINImageReader", "Reads Kodak Cineon (CIN) files." )
 {
 	m_fileNameParameter->setTypedValue(fileName);
 }
 
 CINImageReader::~CINImageReader()
 {
-	delete [] m_buffer;
 }
 
-// void CINImageReader::constructParameters( )
-// {
-// 	// transfer function
-// 	// we should map a string to a functor class
-// 	IntParameter::PresetsMap transferPresets;
-// 	compressionPresets["none"]    = COMPRESSION_NONE;
-// 	compressionPresets["lzw"]     = COMPRESSION_LZW;
-// 	compressionPresets["jpeg"]    = COMPRESSION_JPEG;
-// 	compressionPresets["deflate"] = COMPRESSION_DEFLATE;
-
-// 	m_compressionParameter = new IntParameter("compression", "image data compression method",
-// 															compressionPresets["lzw"], 0, 35535, // min and max magic numbers
-// 															compressionPresets, true);
-// 	parameters()->addParameter(m_compressionParameter);
-// }
-
-
 // partial validity check: assert that the file begins with the CIN magic number
-bool CINImageReader::canRead(const string & fileName)
+bool CINImageReader::canRead( const string & fileName )
 {
 	// attempt to open the file
 	ifstream in(fileName.c_str());
-	if(!in.is_open())
+	if (!in.is_open())
 	{
 		return false;
 	}
 
 	// seek to start
 	in.seekg(0, ios_base::beg);
-	
+
 	// check magic number
 	unsigned int magic;
 	in.read((char *) &magic, sizeof(unsigned int));
 	return magic == 0xd75f2a80 || magic == 0x802a5fd7;
 }
 
-void CINImageReader::channelNames(vector<string> & names)
+void CINImageReader::channelNames( vector<string> &names )
 {
 	names.clear();
-	
+
+	open( true );
+
 	/// \todo read the channel names from the CIN header
 	// form channel names - hardcoded for now, assume RGB (in 10bit log)
 	names.push_back("R");
@@ -123,90 +105,102 @@ void CINImageReader::channelNames(vector<string> & names)
 	names.push_back("B");
 }
 
+
+bool CINImageReader::isComplete()
+{
+	/// \todo
+	return true;
+}
+
+Box2i CINImageReader::dataWindow()
+{
+	open( true );
+
+	return Box2i( V2i( 0, 0 ), V2i( m_bufferWidth - 1, m_bufferHeight - 1 ) );
+}
+
+Box2i CINImageReader::displayWindow()
+{
+	open( true );
+
+	return dataWindow();
+}
+
 /// \todo
 /// we assume here CIN coding in the 'typical' configuration (output by film dumps, nuke, etc).
 /// this is RGB 10bit log for film, pixel-interlaced data.  we convert this to a linear 16-bit (half)
 /// format in the ImagePrimitive.
-void CINImageReader::readChannel(string name, ImagePrimitivePtr image, const Box2i & dataWindow)
-{	
-	if(!open())
+DataPtr CINImageReader::readChannel( const std::string &name, const Imath::Box2i &dataWindow )
+{
+	if (!open())
 	{
-		return;
+		return 0;
 	}
 
-	// compute image extents box
-	Box2i dw;
-	dw.min.x = 0;
-	dw.min.y = 0;
-	dw.max.x = m_bufferWidth-1;
-	dw.max.y = m_bufferHeight-1;
-
-	// determine the image data window
-	Box2i idw = dataWindow.isEmpty() ? dw : dataWindow;
-	image->setDataWindow(idw);
-	image->setDisplayWindow(idw);
-
-	// compute read box
-	Box2i readbox = boxIntersection(dw, idw);
-	
 	// kinda useless here, we have implicitly assumed log 10 bit in the surrounding code
 	int bpp = 10;
-	
+
 	// figure out the offset into the bitstream for the given channel
 	int boffset = name == "R" ? 0 : name == "G" ? 1 : 2;
-	
+
 	// form the mask for this channel
 	unsigned int mask = 0;
-	for(int pi = 0; pi < bpp; ++pi) { mask = 1 + (mask << 1); }
+	for (int pi = 0; pi < bpp; ++pi)
+	{
+		mask = 1 + (mask << 1);
+	}
 	mask <<= ((32 - bpp) - boffset * bpp);
 
-	// copy in the corresponding channel
-	// (convert to half; this datatype has enough room/structure to hold the equivalent
-	//  of the 10bit log values in linear space)
- 	vector<half> &ic = image->createChannel<half>(name)->writable();
+	HalfVectorDataPtr dataContainer = new HalfVectorData();
+	HalfVectorData::ValueType &data = dataContainer->writable();
+	int area = ( dataWindow.size().x + 1 ) * ( dataWindow.size().y + 1 );
+	assert( area >= 0 );
+	data.resize( area );
 
-	// compute distance from the read box origin
-	V2i d = readbox.min - dw.min;
-	
-	// read in the buffer
-	for(int y = readbox.min.y; y <= readbox.max.y; ++y)
+	int dataWidth = 1 + dataWindow.size().x;
+
+	int dataY = 0;
+	for ( int y = dataWindow.min.y - this->dataWindow().min.y ; y <= dataWindow.max.y - this->dataWindow().min.y ; ++y, ++dataY )
 	{
-		for(int x = readbox.min.x; x <= readbox.max.x; ++x)
-		{
-			// i is the index of the pixel on the output image channel
-			int i = (y - idw.min.y) * ( 1 + boxSize( idw ).x ) + (x - idw.min.x);
+		int dataX = 0;
 
-			// di is the index of the pixel in the input image buffer
-			int di = (y - d.y) * ( 1 + boxSize( dw ).x ) + (x - d.x);
-			
-			// get the cell.  for efficiency, we may wish to swap the bytes in the buffer reader
- 			unsigned int cell = reverseBytes(m_buffer[di]);
-			
- 			// assume we have 10bit log, two wasted bits aligning to 32 longword
- 			unsigned short cv = (unsigned short) ((mask & cell) >> (2 + (2 - boffset)*bpp));
-			
-			// convert to a linear floating-point value
- 			ic[i] = m_LUT[cv];
+		for ( int x = dataWindow.min.x - this->dataWindow().min.x;  x <= dataWindow.max.x - this->dataWindow().min.x ; ++x, ++dataX  )
+		{
+			HalfVectorData::ValueType::size_type dataOffset = dataY * dataWidth + dataX;
+			assert( dataOffset < data.size() );
+
+			unsigned int cell = reverseBytes( m_buffer[( y * m_bufferWidth + x )] );
+
+			// assume we have 10bit log, two wasted bits aligning to 32 longword
+			unsigned short cv = (unsigned short) ((mask & cell) >> (2 + (2 - boffset)*bpp));
+
+			data[dataOffset] = m_LUT[ cv ];
 		}
 	}
+
+	return dataContainer;
 }
 
-bool CINImageReader::open()
+bool CINImageReader::open( bool throwOnFailure )
 {
-	if(m_bufferFileName != fileName())
+	if ( fileName() == m_bufferFileName )
+	{
+		return true;
+	}
+
+	try
 	{
 		m_bufferFileName = fileName();
-		delete [] m_buffer;
-		m_buffer = 0;
+		m_buffer.clear();
 
 		ifstream in(m_bufferFileName.c_str());
-		
+
 		// open the file
-		if(!in.is_open())
+		if (!in.is_open())
 		{
 			throw Exception("could not open " + fileName());
 		}
-		
+
 		// read the header
 		FileInformation fi;
 		in.read(reinterpret_cast<char*>(&fi), sizeof(fi));
@@ -225,11 +219,11 @@ bool CINImageReader::open()
 
 		// make sure we have a valid file
 		// 'proper' endianness should have 802A5FD7
-		if(fi.magic != 0xd75f2a80 && fi.magic != 0x802a5fd7)
+		if (fi.magic != 0xd75f2a80 && fi.magic != 0x802a5fd7)
 		{
 			throw Exception("invalid CIN magic number");
 		}
-		
+
 		// tell the image offset
 		fi.image_data_offset = reverseBytes(fi.image_data_offset);
 		fi.industry_header_length = reverseBytes(fi.industry_header_length);
@@ -238,38 +232,29 @@ bool CINImageReader::open()
 		//
 		// image information
 		//
-
-		// dataWindow: use the convention that the image begins at the origin,
-		// and that it extends to (width, height)
-		Box2i dw;
-		dw.min.x = 0;
-		dw.min.y = 0;
-
-		for(int i = 0; i < (int)ii.channel_count; ++i) {
+		for (int i = 0; i < (int)ii.channel_count; ++i)
+		{
 
 			ImageInformationChannelInformation iici = ii.channel_information[i];
 
 			iici.pixels_per_line = reverseBytes(iici.pixels_per_line);
 			iici.lines_per_image = reverseBytes(iici.lines_per_image);
 
-			dw.max.x = iici.pixels_per_line - 1;
-			dw.max.y = iici.lines_per_image - 1;
-
 			m_bufferWidth = iici.pixels_per_line;
 			m_bufferHeight = iici.lines_per_image;
 
-			if(iici.byte_0 == 1)
+			if (iici.byte_0 == 1)
 			{
 				throw Exception("vendor specific CIN files are not handled");
 			}
 
 			// check colour
-			if(!(iici.byte_1 == 1 || iici.byte_1 == 2 || iici.byte_1 == 3))
+			if (!(iici.byte_1 == 1 || iici.byte_1 == 2 || iici.byte_1 == 3))
 			{
 				throw Exception("CIN with non-RGB channel data not handled");
 			}
 		}
-		
+
 		//
 		// device information
 		//
@@ -286,12 +271,12 @@ bool CINImageReader::open()
 		int ref_white_val = 685;
 		double quantization_step = 0.002;
 		double ref_mult = quantization_step / film_gamma;
-		
+
 		// compute black offset
 		double black_offset = pow(10.0, (ref_black_val - ref_white_val) * ref_mult);
-		
+
 		// standard lut
-		for(int i = 0; i < 1024; ++i)
+		for (int i = 0; i < 1024; ++i)
 		{
 			float cvf = (pow(10.0, (i - ref_white_val) * ref_mult) - black_offset) / (1.0 - black_offset);
 			m_LUT[i] = cvf;
@@ -300,17 +285,29 @@ bool CINImageReader::open()
 		//
 		// read in the buffer
 		//
-		
+
 		// determine the size
 		int buffersize = 3 * m_bufferWidth * m_bufferHeight;
 
 		// assume data is interlaced, just read the whole thing,
 		// then stripe off the channel
-		m_buffer = new unsigned int[buffersize];
+		m_buffer.resize( buffersize );
 
 		// read the data into the buffer
-		in.read((char *) m_buffer, sizeof(unsigned int) * buffersize);
+		in.read((char *)(&m_buffer[0]), sizeof(unsigned int) * buffersize);
 	}
-	
-	return m_buffer;
+	catch (...)
+	{
+		if ( throwOnFailure )
+		{
+			throw;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	return true;
 }
+
