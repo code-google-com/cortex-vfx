08c8d10a133b389ebf4cbccc4876dbb20b673ea0
lucio.imageengine <lucio.imageengine@07b78fde-9e1a-0410-98de-79dcf2039bee>
2008-06-13 00:39:07 +0000

    tagging IECore 2.41.0

---
+++ tags/2.41.0/src/IECore/MeshPrimitiveShrinkWrapOp.cpp
@@ -0,0 +1,405 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2008, Image Engine Design Inc. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+//     * Neither the name of Image Engine Design nor the names of any
+//       other contributors to this software may be used to endorse or
+//       promote products derived from this software without specific prior
+//       written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+//  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+//  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+//  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "boost/format.hpp"
+
+#include "IECore/ObjectParameter.h"
+#include "IECore/CompoundParameter.h"
+#include "IECore/CompoundObject.h"
+#include "IECore/VectorTypedData.h"
+#include "IECore/RunTimeTyped.h"
+#include "IECore/MeshPrimitiveShrinkWrapOp.h"
+#include "IECore/PrimitiveEvaluator.h"
+#include "IECore/VectorOps.h"
+#include "IECore/TriangulateOp.h"
+#include "IECore/ClassData.h"
+
+using namespace IECore;
+using namespace Imath;
+
+struct MeshPrimitiveShrinkWrapOp::ExtraData
+{
+	MeshPrimitiveParameterPtr m_directionMeshParameter;
+	FloatParameterPtr m_triangulationToleranceParameter;
+};
+
+typedef ClassData< MeshPrimitiveShrinkWrapOp, MeshPrimitiveShrinkWrapOp::ExtraData*, Deleter<MeshPrimitiveShrinkWrapOp::ExtraData*> > MeshPrimitiveShrinkWrapOpClassData;
+static MeshPrimitiveShrinkWrapOpClassData g_classData;
+
+MeshPrimitiveShrinkWrapOp::MeshPrimitiveShrinkWrapOp() : MeshPrimitiveOp( staticTypeName(), "A MeshPrimitiveOp to shrink-wrap one mesh onto another" )
+{
+	ExtraData *extraData = g_classData.create( this, new ExtraData() );
+	assert( extraData );
+
+	m_targetMeshParameter = new MeshPrimitiveParameter(
+	        "target",
+	        "The target mesh to shrink-wrap onto.",
+	        new MeshPrimitive()
+	);
+
+	IntParameter::PresetsMap directionPresets;
+	directionPresets["Inside"] = Inside;
+	directionPresets["Outside"] = Outside;
+	directionPresets["Both"] = Both;
+
+	m_directionParameter = new IntParameter(
+	        "direction",
+	        "The direction by which rays are cast to find the target mesh.",
+	        Both,
+	        Inside,
+	        Both,
+	        directionPresets,
+	        true
+	);
+	
+	IntParameter::PresetsMap methodPresets;
+	methodPresets["Normal"] = Normal;
+	methodPresets["X-axis"] = XAxis;
+	methodPresets["Y-axis"] = YAxis;
+	methodPresets["Z-axis"] = ZAxis;		
+	methodPresets["DirectionMesh"] = DirectionMesh;	
+
+	m_methodParameter = new IntParameter(
+	        "method",
+	        "The method by which find the target mesh.",
+	        Normal,
+	        Normal,
+	        DirectionMesh,
+	        methodPresets,
+	        true
+	);
+	
+	
+	extraData->m_directionMeshParameter = new MeshPrimitiveParameter(
+	        "directionMesh",
+	        "The direction mesh to use when determining where to cast rays",
+	        new MeshPrimitive()
+	);
+	
+	extraData->m_triangulationToleranceParameter = new FloatParameter(
+		"triangulationTolerance",
+		"Set the non-planar and non-convex tolerance for the internal triangulation tests",
+		1.e-6f,
+		0.0f
+	);
+
+	parameters()->addParameter( m_targetMeshParameter );
+	parameters()->addParameter( m_directionParameter );
+	parameters()->addParameter( m_methodParameter );
+	parameters()->addParameter( extraData->m_directionMeshParameter );
+	parameters()->addParameter( extraData->m_triangulationToleranceParameter );
+}
+
+MeshPrimitiveShrinkWrapOp::~MeshPrimitiveShrinkWrapOp()
+{
+	g_classData.erase( this );
+}
+
+MeshPrimitiveParameterPtr MeshPrimitiveShrinkWrapOp::targetMeshParameter()
+{
+	return m_targetMeshParameter;
+}
+
+ConstMeshPrimitiveParameterPtr MeshPrimitiveShrinkWrapOp::targetMeshParameter() const
+{
+	return m_targetMeshParameter;
+}
+
+IntParameterPtr MeshPrimitiveShrinkWrapOp::methodParameter()
+{
+	return m_methodParameter;
+}
+
+ConstIntParameterPtr MeshPrimitiveShrinkWrapOp::methodParameter() const
+{
+	return m_methodParameter;
+}
+
+IntParameterPtr MeshPrimitiveShrinkWrapOp::directionParameter()
+{
+	return m_directionParameter;
+}
+
+ConstIntParameterPtr MeshPrimitiveShrinkWrapOp::directionParameter() const
+{
+	return m_directionParameter;
+}
+
+MeshPrimitiveParameterPtr MeshPrimitiveShrinkWrapOp::directionMeshParameter()
+{
+	ExtraData *extraData = g_classData[this];
+	assert( extraData );		
+	return extraData->m_directionMeshParameter;
+}
+
+ConstMeshPrimitiveParameterPtr MeshPrimitiveShrinkWrapOp::directionMeshParameter() const
+{
+	ExtraData *extraData = g_classData[this];
+	assert( extraData );		
+	return extraData->m_directionMeshParameter;
+}
+
+FloatParameterPtr MeshPrimitiveShrinkWrapOp::triangulationToleranceParameter()
+{
+	ExtraData *extraData = g_classData[this];
+	assert( extraData );		
+	return extraData->m_triangulationToleranceParameter;
+}
+
+ConstFloatParameterPtr MeshPrimitiveShrinkWrapOp::triangulationToleranceParameter() const
+{
+	ExtraData *extraData = g_classData[this];
+	assert( extraData );		
+	return extraData->m_triangulationToleranceParameter;
+}
+
+template<typename T>
+void MeshPrimitiveShrinkWrapOp::doShrinkWrap( std::vector<T> &vertices, PrimitivePtr sourceMesh, ConstPrimitivePtr targetMesh, typename TypedData< std::vector<T> >::ConstPtr directionVerticesData, Direction direction, Method method )
+{
+	assert( sourceMesh );
+
+	if ( ! targetMesh )
+	{
+		return;
+	}
+	
+	if ( method == DirectionMesh  )
+	{
+		if ( !directionVerticesData )
+		{
+			throw InvalidArgumentException( (boost::format("Direction mesh has no primitive variable \"P\" of type %s in MeshPrimitiveShrinkWrapOp") % TypedData< std::vector<T> >::staticTypeName() ).str() );
+		}
+		else if ( directionVerticesData->readable().size() != vertices.size() )
+		{
+			throw InvalidArgumentException("Direction mesh has incorrect number of vertices in MeshPrimitiveShrinkWrapOp" );
+		}
+	}
+
+	TriangulateOpPtr op = new TriangulateOp();
+	op->inputParameter()->setValue( sourceMesh );
+	op->toleranceParameter()->setNumericValue( this->triangulationToleranceParameter()->getNumericValue() );
+	MeshPrimitivePtr triangulatedSourcePrimitive = runTimeCast< MeshPrimitive > ( op->operate() );
+
+	PrimitiveEvaluatorPtr sourceEvaluator = 0;
+	PrimitiveEvaluator::ResultPtr sourceResult = 0;
+	
+	if ( method == Normal )
+	{
+		sourceEvaluator = PrimitiveEvaluator::create( triangulatedSourcePrimitive );
+		assert( sourceEvaluator );
+		sourceResult = sourceEvaluator->createResult();
+		assert( sourceResult );
+	}
+
+	PrimitiveEvaluatorPtr targetEvaluator = PrimitiveEvaluator::create( targetMesh );
+	assert( targetEvaluator );
+	PrimitiveEvaluator::ResultPtr insideResult = targetEvaluator->createResult();
+	PrimitiveEvaluator::ResultPtr outsideResult = targetEvaluator->createResult();
+	assert( insideResult );
+	assert( outsideResult );
+
+	PrimitiveVariableMap::const_iterator it = triangulatedSourcePrimitive->variables.find( "N" );
+	if (it == sourceMesh->variables.end())
+	{
+		throw InvalidArgumentException("MeshPrimitive has no primitive variable \"N\" in MeshPrimitiveShrinkWrapOp" );
+	}
+
+	const PrimitiveVariable &nPrimVar = it->second;
+
+	typename std::vector<T>::size_type vertexId = 0;
+	for ( typename std::vector<T>::iterator pit = vertices.begin(); pit != vertices.end(); ++pit, ++vertexId )
+	{
+		T &vertexPosition = *pit;
+
+		T rayDirection;
+		
+		if ( method == Normal )
+		{
+			assert( sourceEvaluator );
+			assert( sourceResult );
+			sourceEvaluator->closestPoint( vertexPosition, sourceResult );
+			rayDirection = sourceResult->vectorPrimVar( nPrimVar ).normalized();
+		} 
+		else if ( method == XAxis )
+		{
+			rayDirection = T( 1.0, 0.0, 0.0 );
+		}
+		else if ( method == YAxis )
+		{
+			rayDirection = T( 0.0, 1.0, 0.0 );
+		}
+		else if ( method == ZAxis )
+		{
+			rayDirection = T( 0.0, 0.0, 1.0 );
+		}
+		else 
+		{
+			assert( method == DirectionMesh );
+			assert( directionVerticesData );
+			assert( vertexId < directionVerticesData->readable().size() );
+			
+			rayDirection = ( directionVerticesData->readable()[ vertexId ] - vertexPosition ).normalized();	
+		}
+
+		bool hit = false;
+
+		if ( direction == Inside )
+		{
+			hit = targetEvaluator->intersectionPoint( vertexPosition, -rayDirection, insideResult );
+			if ( hit )
+			{
+				vertexPosition = insideResult->point();
+			}
+		}
+		else if ( direction == Outside )
+		{
+			hit = targetEvaluator->intersectionPoint( vertexPosition, rayDirection, outsideResult );
+			if ( hit )
+			{
+				vertexPosition = outsideResult->point();
+			}
+		}
+		else
+		{
+			assert( direction == Both );
+
+			bool insideHit  = targetEvaluator->intersectionPoint( vertexPosition, -rayDirection, insideResult  );
+			bool outsideHit = targetEvaluator->intersectionPoint( vertexPosition,  rayDirection, outsideResult );
+
+			/// Choose the closest, or the only, intersection
+			if ( insideHit && outsideHit )
+			{
+				typename T::BaseType insideDist  = vecDistance2( vertexPosition, T( insideResult->point()  ) );
+				typename T::BaseType outsideDist = vecDistance2( vertexPosition, T( outsideResult->point() ) );
+
+				if ( insideDist < outsideDist )
+				{
+					vertexPosition = insideResult->point();
+				}
+				else
+				{
+					vertexPosition = outsideResult->point();
+				}
+			}
+			else if ( insideHit )
+			{
+				vertexPosition = insideResult->point();
+			}
+			else if ( outsideHit )
+			{
+				vertexPosition = outsideResult->point();
+			}
+		}
+	}
+}
+
+void MeshPrimitiveShrinkWrapOp::modifyTypedPrimitive( MeshPrimitivePtr mesh, ConstCompoundObjectPtr operands )
+{
+	assert( mesh );
+	assert( operands );
+	
+	PrimitiveVariableMap::const_iterator it = mesh->variables.find("P");
+	if (it == mesh->variables.end())
+	{
+		throw InvalidArgumentException("MeshPrimitive has no primitive variable \"P\" in MeshPrimitiveShrinkWrapOp" );
+	}
+	const DataPtr &verticesData = it->second.data;
+
+	if (! mesh->arePrimitiveVariablesValid() )
+	{
+		throw InvalidArgumentException( "Mesh with invalid primitive variables given to MeshPrimitiveShrinkWrapOp" );
+	}
+
+	MeshPrimitivePtr target = targetMeshParameter()->getTypedValue< MeshPrimitive >( );
+	if ( !target )
+	{
+		return;
+	}
+
+	if ( ! target->arePrimitiveVariablesValid() )
+	{
+		throw InvalidArgumentException( "Target mesh with invalid primitive variables given to MeshPrimitiveShrinkWrapOp" );
+	}
+
+	TriangulateOpPtr op = new TriangulateOp();
+	op->inputParameter()->setValue( target );
+	target = runTimeCast< MeshPrimitive > ( op->operate() );
+	assert( target );
+
+	Direction direction = static_cast<Direction>( m_directionParameter->getNumericValue() );
+	Method method = static_cast<Method>( m_methodParameter->getNumericValue() );
+	
+	MeshPrimitivePtr directionMesh = 0;
+	
+	ConstDataPtr directionVerticesData = 0;
+	if ( method == DirectionMesh )
+	{
+		directionMesh = directionMeshParameter()->getTypedValue< MeshPrimitive >( );
+				
+		if ( ! directionMesh )
+		{
+			throw InvalidArgumentException( "No direction mesh given to MeshPrimitiveShrinkWrapOp" );
+		}
+		
+		if ( ! directionMesh->arePrimitiveVariablesValid() )
+		{
+			throw InvalidArgumentException( "Direction mesh with invalid primitive variables given to MeshPrimitiveShrinkWrapOp" );
+		}
+		
+		PrimitiveVariableMap::const_iterator it = directionMesh->variables.find("P");
+		if (it == directionMesh->variables.end())
+		{
+			throw InvalidArgumentException("Direction mesh has no primitive variable \"P\" in MeshPrimitiveShrinkWrapOp" );
+		}
+		
+		directionVerticesData = it->second.data;	
+	}
+	
+	
+	/// \todo Start using depatchTypedData
+	if (runTimeCast<V3fVectorData>(verticesData))
+	{
+		V3fVectorDataPtr p = runTimeCast<V3fVectorData>(verticesData);
+		doShrinkWrap<V3f>( p->writable(), mesh, target, runTimeCast<const V3fVectorData>(directionVerticesData), direction, method );
+	}
+	else if (runTimeCast<V3dVectorData>(verticesData))
+	{
+		V3dVectorDataPtr p = runTimeCast<V3dVectorData>(verticesData);
+		doShrinkWrap<V3d>( p->writable(), mesh, target, runTimeCast<const V3dVectorData>(directionVerticesData), direction, method );
+	}
+	else
+	{
+		throw InvalidArgumentException("MeshPrimitive has no primitive variable \"P\" of type V3fVectorData/V3dVectorData in MeshPrimitiveShrinkWrapOp" );
+	}
+}
---
+++ tags/2.41.0/test/IECoreRI/Renderer.py
@@ -0,0 +1,346 @@
+##########################################################################
+#
+#  Copyright (c) 2007-2008, Image Engine Design Inc. All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are
+#  met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+#     * Neither the name of Image Engine Design nor the names of any
+#       other contributors to this software may be used to endorse or
+#       promote products derived from this software without specific prior
+#       written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+#  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+#  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+#  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+#  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+#  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+#  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+#  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+##########################################################################
+
+import unittest
+import os
+from IECore import *
+import IECoreRI
+
+class SimpleProcedural( Renderer.Procedural ) :
+
+	def __init__( self, scale ) :
+	
+		Renderer.Procedural.__init__( self, "name", "description" )
+		self.__scale = scale
+		self.__t = StringData( "hello" )
+		self.__c = CompoundData()
+		self.__c["a"] = IntData( 4 )
+
+	def doBound( self, args ) :
+	
+		return Box3f( V3f( -self.__scale ), V3f( self.__scale ) )
+		
+	def doRender( self, renderer, args ) :
+	
+		renderer.transformBegin()
+		
+		m = M44f()
+		m.scale( V3f( self.__scale ) )
+		renderer.concatTransform( m )
+		
+		renderer.transformEnd()
+		
+
+class RendererTest( unittest.TestCase ) :
+
+	def loadShader( self, shader ) :
+	
+		return IECoreRI.SLOReader( os.path.join( os.environ["SHADER_PATH"], shader + ".sdl" ) ).read()
+
+	def testTypeName( self ) :
+	
+		r = IECoreRI.Renderer()
+		self.assertEqual( r.typeName(), "IECoreRI::Renderer" )
+
+	## \todo Make this test actually test something
+	def testNoContext( self ) :
+	
+		r = IECoreRI.Renderer()
+
+	## \todo Make this test actually test something
+	def test( self ) :
+	
+		r = IECoreRI.Renderer( "test/IECoreRI/output/test.rib" )
+		
+		r.setOption( "ri:searchpath:shader", StringData( os.environ["SHADER_PATH"] ) )
+		r.setOption( "ri:render:bucketorder", StringData( "zigzag" ) )
+		r.setOption( "user:magicNumber", IntData( 42 ) )
+		r.setOption( "ri:pixelSamples", V2iData( V2i( 8, 8 ) ) )
+		
+		r.worldBegin()
+		
+		r.transformBegin()
+		r.attributeBegin()
+		
+		self.loadShader( "plastic" ).render( r )
+		
+		Reader.create( "test/IECoreRI/data/sphere.cob" ).read().render( r )
+		
+		r.attributeEnd()
+		r.transformEnd()
+		
+		r.worldEnd()
+
+	def testAttributes( self ) :
+	
+		tests = [
+			# format is : name value expectedRib getAttributeShouldWork
+			( "ri:shadingRate", FloatData( 2 ), "ShadingRate 2", True ),
+			( "ri:matte", BoolData( 0 ), "Matte 0", True ),
+			( "ri:matte", BoolData( 1 ), "Matte 1", True ),
+			( "user:whatever", StringData( "whatever" ), "Attribute \"user\" \"string whatever\" [ \"whatever\" ]", True ),
+			( "ri:color", Color3fData( Color3f( 0, 1, 1 ) ), "Color [ 0 1 1 ]", False ),
+			( "color", Color3fData( Color3f( 1, 2, 3 ) ), "Color [ 1 2 3 ]", False ),
+			( "ri:opacity", Color3fData( Color3f( 1, 1, 1 ) ), "Opacity [ 1 1 1 ]", False ),
+			( "opacity", Color3fData( Color3f( 0, 1, 0 ) ), "Opacity [ 0 1 0 ]", False ),
+			( "ri:sides", IntData( 1 ), "Sides 1", False ),
+			( "ri:geometricApproximation:motionFactor", FloatData( 1 ), "GeometricApproximation \"motionfactor\" 1", False ),
+			( "ri:geometricApproximation:focusFactor", FloatData( 1 ), "GeometricApproximation \"focusfactor\" 1", False ),
+			( "ri:cull:hidden", IntData( 0 ), "Attribute \"cull\" \"int hidden\" [ 0 ]", False ),
+			( "name", StringData( "oioi" ), "Attribute \"identifier\" \"string name\" [ \"oioi\" ]", True ),
+			( "ri:trace:bias", FloatData( 2 ), "Attribute \"trace\" \"float bias\" [ 2 ]", True ),
+		]
+		
+		for t in tests :
+		
+			r = IECoreRI.Renderer( "test/IECoreRI/output/testAttributes.rib" )
+			r.worldBegin()
+			r.setAttribute( t[0], t[1] )
+			if t[3] :
+				self.assertEqual( r.getAttribute( t[0] ), t[1] )
+			r.worldEnd()
+			
+			l = "".join( file( "test/IECoreRI/output/testAttributes.rib" ).readlines() )
+			l = " ".join( l.split() )
+			self.assert_( t[2] in l )
+
+		
+	## \todo Make this test actually test something
+	def testProcedural( self ) :
+	
+		r = IECoreRI.Renderer( "test/IECoreRI/output/testProcedural.rib" )
+		r.worldBegin()
+		
+		r.procedural( SimpleProcedural( 10.5 ) )
+		
+		r.worldEnd()
+		
+	def testGetOption( self ) :
+	
+		r = IECoreRI.Renderer( "test/IECoreRI/output/testGetOption.rib" )
+		
+		r.camera( "main", { "resolution" : V2iData( V2i( 1024, 768 ) ) } )
+		
+		r.worldBegin()
+		
+		s = r.getOption( "shutter" )
+		self.assertEqual( s, V2fData( V2f( 0 ) ) )
+		
+		self.assertEqual( r.getOption( "camera:resolution" ), V2iData( V2i( 1024, 768 ) ) )
+		
+		r.worldEnd()
+		
+	## \todo Make this test actually test something
+	def testDisplay( self ) :
+	
+		r = IECoreRI.Renderer( "test/IECoreRI/output/testDisplay.rib" )
+		
+		r.display( "test.tif", "tiff", "rgba", { "quantize" : FloatVectorData( [ 0, 1, 0, 1 ] ) } )
+		
+		r.worldBegin()
+		r.worldEnd()
+		
+	## \todo Make this test actually test something
+	def testSubDivs( self ) :
+	
+		r = IECoreRI.Renderer( "test/IECoreRI/output/subdiv.rib" )
+		
+		r.display( "test", "idisplay", "rgba", {} )
+		
+		r.worldBegin()
+		
+		t = M44f()
+		t.translate( V3f( 0, 0, 10 ) )
+		r.concatTransform( t )
+		m = ObjectReader( "test/IECoreRI/data/openSubDivCube.cob" ).read()
+		m.render( r )
+		
+		r.worldEnd()
+	
+	## \todo Make this test actually test something
+	def testCommands( self ) :
+	
+		r = IECoreRI.Renderer( "test/IECoreRI/output/commands.rib" )
+		
+		r.worldBegin()
+		
+		r.command( "ri:readArchive", { "name" : StringData( "nameOfArchive" ) } )
+		
+		r.worldEnd()
+		
+	## \todo Make this test actually test something
+	def testMotion( self ) :
+	
+		r = IECoreRI.Renderer( "test/IECoreRI/output/motion.rib" )
+		
+		r.worldBegin()
+		
+		m = MatrixMotionTransform()
+		m[0] = M44f.createTranslated( V3f( 0, 1, 0 ) )
+		m[1] = M44f.createTranslated( V3f( 0, 10, 0 ) )
+		
+		m.render( r )
+		
+		r.worldEnd()	
+	
+	## \todo Make this test actually test something
+	def testStringPrimVars( self ) :
+	
+		r = IECoreRI.Renderer( "test/IECoreRI/output/stringPrimVars.rib" )	
+		
+		r.worldBegin()
+		
+		m = ObjectReader( "test/IECoreRI/data/stringPrimVars.cob" ).read()
+		m.render( r )
+		
+		r.worldEnd()
+		
+	def testGetTransform( self ) :
+	
+		r = IECoreRI.Renderer( "test/IECoreRI/output/transform.rib" )
+		
+		r.worldBegin()
+		
+		self.assertEqual( r.getTransform(), M44f() )
+		self.assertEqual( r.getTransform( "world" ), M44f() )
+		self.assertEqual( r.getTransform( "object" ), M44f() )
+		
+		r.transformBegin()
+		
+		t = M44f.createTranslated( V3f( 1, 2, 3 ) ) * M44f.createScaled( V3f( 2, 1, 0 ) ) * M44f.createRotated( V3f( 20, 0, 90 ) )
+		r.concatTransform( t )
+		self.assert_( r.getTransform( "object" ).equalWithAbsError( t, 0.000001 ) )
+		self.assert_( r.getTransform().equalWithAbsError( t, 0.000001 ) )
+		
+		r.coordinateSystem( "coordSys" )
+		self.assert_( r.getTransform( "coordSys" ).equalWithAbsError( t, 0.000001 ) )
+		
+		r.transformEnd()
+		
+		self.assertEqual( r.getTransform(), M44f() )
+		self.assertEqual( r.getTransform( "world" ), M44f() )
+		self.assertEqual( r.getTransform( "object" ), M44f() )
+		self.assert_( r.getTransform( "coordSys" ).equalWithAbsError( t, 0.000001 ) )
+		
+		r.worldEnd()
+		
+	## \todo Make this test actually work non-interactively - perhaps install a custom message handler
+	# to ensure that no messages are output during the setAttribute call, and check the rib for unwanted output.
+	def testIgnoreOtherAttributesAndOptions( self ) :
+	
+		r = IECoreRI.Renderer( "test/IECoreRI/output/transform.rib" )
+		
+		r.setOption( "someOthereRenderer:someOtherOption", IntData( 10 ) )
+		
+		r.worldBegin()
+		
+		# this should be silently ignored
+		r.setAttribute( "someOtherRenderer:someOtherAttribute", IntData( 10 ) )
+		
+		r.worldEnd()
+	
+	def testMissingShaders( self ) :
+	
+		"""Check that missing shaders don't throw an exception but print a message instead."""
+	
+		r = IECoreRI.Renderer( "test/IECoreRI/output/missingShaders.rib" )
+		
+		r.worldBegin()
+		
+		r.shader( "surface", "aShaderWhichDoesntExist", {} )
+		
+		r.worldEnd()
+		
+	def testGetUserOption( self ) :
+	
+		r = IECoreRI.Renderer( "test/IECoreRI/output/getUserOption.rib" )
+		
+		o = {
+			"user:f" : FloatData( 10 ),
+			"user:i" : IntData( 100 ),
+			"user:s" : StringData( "hello" ),
+			"user:c" : Color3fData( Color3f( 1, 0, 0 ) ),
+			"user:v" : V3fData( V3f( 1, 2, 3 ) ),
+			"user:m" : M44fData( M44f.createTranslated( V3f( 1, 2, 3 ) ) ),
+		}
+		
+		for k, v in o.items() :
+		
+			r.setOption( k, v )
+			self.assertEqual( r.getOption( k ), v )
+			
+	def testGetUserAttribute( self ) :
+	
+		r = IECoreRI.Renderer( "test/IECoreRI/output/getUserAttribute.rib" )
+		
+		o = {
+			"user:f" : FloatData( 10 ),
+			"user:i" : IntData( 100 ),
+			"user:s" : StringData( "hello" ),
+			"user:c" : Color3fData( Color3f( 1, 0, 0 ) ),
+			"user:v" : V3fData( V3f( 1, 2, 3 ) ),
+			"user:m" : M44fData( M44f.createTranslated( V3f( 1, 2, 3 ) ) ),
+		}
+		
+		for k, v in o.items() :
+		
+			r.setAttribute( k, v )
+			self.assertEqual( r.getAttribute( k ), v )
+			
+	def tearDown( self ) :
+	
+		files = [
+			"test/IECoreRI/output/test.rib",
+			"test/IECoreRI/output/testAttributes.rib",
+			"test/IECoreRI/output/testProcedural.rib",
+			"test/IECoreRI/output/testGetOption.rib",
+			"test/IECoreRI/output/testDisplay.rib",
+			"test/IECoreRI/output/testCamera.rib",
+			"test/IECoreRI/output/subdiv.rib",
+			"test/IECoreRI/output/commands.rib",
+			"test/IECoreRI/output/motion.rib",
+			"test/IECoreRI/output/stringPrimVars.rib",
+			"test/IECoreRI/output/transform.rib",
+			"test/IECoreRI/output/missingShaders.rib",
+			"test/IECoreRI/output/getUserOption.rib",
+			"test/IECoreRI/output/getUserAttribute.rib",
+		]
+		
+		for f in files :
+			if os.path.exists( f ):
+				os.remove( f )		
+			
+		
+if __name__ == "__main__":
+    unittest.main()   
