9dcbeabccd848f42c02a1ab580c64c06b4f736a1
lucio.imageengine <lucio.imageengine@07b78fde-9e1a-0410-98de-79dcf2039bee>
2010-11-04 19:11:07 +0000

    Tagging Cortex 5.23.0

---
+++ tags/5.23.0/contrib/IECoreHoudini/python/IECoreHoudini/FnProceduralHolder.py
@@ -0,0 +1,100 @@
+##########################################################################
+#
+#  Copyright 2010 Dr D Studios Pty Limited (ACN 127 184 954) (Dr. D Studios),
+#  its affiliates and/or its licensors.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are
+#  met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+#     * Neither the name of Image Engine Design nor the names of any
+#       other contributors to this software may be used to endorse or
+#       promote products derived from this software without specific prior
+#       written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+#  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+#  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+#  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+#  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+#  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+#  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+#  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+##########################################################################
+
+import hou
+import IECore
+import IECoreHoudini
+from IECoreHoudini.FnParameterisedHolder import FnParameterisedHolder
+
+class FnProceduralHolder(FnParameterisedHolder):
+
+	# create our function set and stash which node we're looking at
+	def __init__(self, node=None):
+		FnParameterisedHolder.__init__(self, node)
+
+	@classmethod
+	def create(cls, name, type, version, path="IECORE_PROCEDURAL_PATHS"):
+		obj = hou.node("/obj")
+		geo = obj.createNode("geo", node_name=name, run_init_scripts=False)
+		proc = geo.createNode( "ieProceduralHolder", node_name=name )
+		fn = IECoreHoudini.FnProceduralHolder( proc )
+		cl = IECore.ClassLoader.defaultLoader( path ).load( type, version )
+		fn.setParameterised( cl() )
+		return proc
+
+	# do we have a valid parameterised instance?
+	def hasParameterised(self):
+		if not self.nodeValid():
+			return False
+		return IECoreHoudini._IECoreHoudini._FnProceduralHolder(self.node()).hasParameterised()
+
+	# this sets a procedural on our node and then updates the parameters
+	def setParameterised(self, procedural, refresh_gui=True ):
+		if not self.nodeValid():
+			return
+		fn = IECoreHoudini._IECoreHoudini._FnProceduralHolder(self.node())
+
+		# get our procedural type/version which is added by ClassLoader
+		type = "Unknown"
+		version = "Unknown"
+		if hasattr(procedural, "path"):
+			type = procedural.path
+		if hasattr(procedural, "version"):
+			version = procedural.version
+
+		# update the procedural on our SOP & refresh the gui
+		fn.setParameterised( procedural, type, version )
+
+		# refresh our parameters
+		if refresh_gui:
+			self.updateParameters( procedural )
+
+	# this returns the procedural our node is working with
+	def getParameterised(self):
+		if self.nodeValid():
+			if IECoreHoudini._IECoreHoudini._FnProceduralHolder(self.node()).hasParameterised():
+				return IECoreHoudini._IECoreHoudini._FnProceduralHolder(self.node()).getParameterised()
+		return None
+
+	# forces the list of cached class names to refresh	
+	def refreshClassNames(self):
+		if self.nodeValid():
+			IECoreHoudini._IECoreHoudini._FnProceduralHolder(self.node()).refreshClassNames()
+		
+	# get our list of class names based on matchString
+	def classNames(self):
+		if self.nodeValid():
+			return IECoreHoudini._IECoreHoudini._FnProceduralHolder(self.node()).classNames()
+		return None
