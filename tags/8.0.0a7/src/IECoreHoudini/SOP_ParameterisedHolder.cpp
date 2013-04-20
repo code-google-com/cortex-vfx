8d7323c3e89d2278f179ef2c9d12e5ed1fe8f883
john.imageengine <john.imageengine@07b78fde-9e1a-0410-98de-79dcf2039bee>
2013-04-20 02:51:45 +0000

    Tagging version 8.0.0a7.

---
+++ tags/8.0.0a7/src/IECoreHoudini/SOP_ParameterisedHolder.cpp
@@ -0,0 +1,250 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  Copyright 2010 Dr D Studios Pty Limited (ACN 127 184 954) (Dr. D Studios),
+//  its affiliates and/or its licensors.
+//
+//  Copyright (c) 2010-2013, Image Engine Design Inc. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+//     * Neither the name of Image Engine Design nor the names of any
+//       other contributors to this software may be used to endorse or
+//       promote products derived from this software without specific prior
+//       written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+//  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+//  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+//  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "boost/bind.hpp"
+#include "boost/python.hpp"
+
+#include "GA/GA_AIFBlindData.h"
+
+#include "IECore/CapturingRenderer.h"
+#include "IECore/Group.h"
+#include "IECore/Op.h"
+#include "IECore/ObjectParameter.h"
+#include "IECore/ParameterisedProcedural.h"
+#include "IECore/WorldBlock.h"
+
+#include "IECorePython/ScopedGILLock.h"
+#include "IECorePython/ScopedGILRelease.h"
+
+#include "IECoreHoudini/FromHoudiniGeometryConverter.h"
+#include "IECoreHoudini/NodePassData.h"
+#include "IECoreHoudini/SOP_ParameterisedHolder.h"
+
+using namespace boost;
+using namespace boost::python;
+
+using namespace IECore;
+using namespace IECoreHoudini;
+
+SOP_ParameterisedHolder::SOP_ParameterisedHolder( OP_Network *net, const char *name, OP_Operator *op ) : ParameterisedHolder<SOP_Node>( net, name, op )
+{
+	IECoreHoudini::MessageHandler::HandlerFn errorFn = boost::bind( &SOP_ParameterisedHolder::addError, this, SOP_MESSAGE, _1 );
+	IECoreHoudini::MessageHandler::HandlerFn warningFn = boost::bind( &SOP_ParameterisedHolder::addWarning, this, SOP_MESSAGE, _1 );
+	IECoreHoudini::MessageHandler::HandlerFn infoFn = boost::bind( &SOP_ParameterisedHolder::addMessage, this, SOP_MESSAGE, _1 );
+	IECore::MessageHandlerPtr h = new IECoreHoudini::MessageHandler( errorFn, warningFn, infoFn, infoFn );
+	setMessageHandler( h );
+}
+
+SOP_ParameterisedHolder::~SOP_ParameterisedHolder()
+{
+}
+
+void SOP_ParameterisedHolder::refreshInputConnections()
+{
+	// store the existing input nodes
+	unsigned numInputs = nInputs();
+	std::vector<OP_Node*> inputNodes;
+	for ( unsigned i=0; i < numInputs; i++ )
+	{
+		OP_Node *input = getInput( i );
+		if ( !input )
+		{
+			continue;
+		}
+		
+		inputNodes.push_back( input );
+	}
+	
+	// clear the input parameters and break the node connections
+	disconnectAllInputs();
+	m_inputParameters.clear();
+
+	ParameterisedInterface *parameterised = getParameterisedInterface();
+	if ( !parameterised )
+	{
+		return;
+	}
+
+	// add inputs for the appropriate parameters
+	const CompoundParameter::ParameterVector &parameters = parameterised->parameters()->orderedParameters();
+	for ( CompoundParameter::ParameterVector::const_iterator it=parameters.begin(); it!=parameters.end(); it++ )
+	{
+		// adding ObjectParameters explicitly so generic data can be passed through
+		// ParameterisedHolders even if they don't make particular sense in SOPs.
+		if ( (*it)->typeId() == ObjectParameterTypeId )
+		{
+			m_inputParameters.push_back( *it );
+		}
+		else
+		{
+			const ObjectParameter *objectParam = IECore::runTimeCast<ObjectParameter>( *it );
+			if ( !objectParam )
+			{
+				continue;
+			}
+			
+			FromHoudiniGeometryConverterPtr converter = FromHoudiniGeometryConverter::create( objectParam->validTypes() );
+			if ( converter )
+			{
+				m_inputParameters.push_back( *it );
+				/// \todo: add converter parameters here once ParameterHandlers are defined in c++
+			}
+		}
+		
+		/// \todo: try using the messageHandler(). it might not work outside of a cook though...
+		if ( m_inputParameters.size() > 4 )
+		{
+			std::cerr << "ieParameterisedHolder does not support more than 4 input parameter connections." << std::endl;
+		}
+	}
+
+	// remake the connections to inputs that still exist
+	numInputs = std::min( inputNodes.size(), m_inputParameters.size() );
+	for ( unsigned i=0; i < numInputs; i++ )
+	{
+		setInput( i, inputNodes[i] );
+	}
+	
+	/// \todo: Is this really the only we can get the gui to update the input connections?
+	setXY( getX()+.0001, getY()+.0001 );
+	setXY( getX()-.0001, getY()-.0001 );
+}
+
+void SOP_ParameterisedHolder::setInputParameterValues( float now )
+{
+	for ( unsigned int i=0; i < m_inputParameters.size(); i++ )
+	{
+		useInputSource( i, m_dirty, false );
+		
+		IECore::ParameterPtr inputParameter = m_inputParameters[i];
+		
+		GU_DetailHandle inputHandle = inputGeoHandle( i );
+		GU_DetailHandleAutoReadLock readHandle( inputHandle );
+		const GU_Detail *inputGdp = readHandle.getGdp();
+		if ( !inputGdp )
+		{
+			continue;
+		}
+		
+		const GA_ROAttributeRef attrRef = inputGdp->findAttribute( GA_ATTRIB_DETAIL, GA_SCOPE_PRIVATE, "IECoreHoudiniNodePassData" );
+		if ( attrRef.isValid() )
+		{
+			// looks like data passed from another ParameterisedHolder
+			const GA_Attribute *attr = attrRef.getAttribute();
+			const GA_AIFBlindData *blindData = attr->getAIFBlindData();
+			const NodePassData passData = blindData->getValue<NodePassData>( attr, 0 );
+			SOP_ParameterisedHolder *sop = dynamic_cast<SOP_ParameterisedHolder*>( const_cast<OP_Node*>( passData.nodePtr() ) );
+			
+			IECore::ConstObjectPtr result = 0;
+			if ( passData.type() == IECoreHoudini::NodePassData::CORTEX_OPHOLDER )
+			{
+				IECore::OpPtr op = IECore::runTimeCast<IECore::Op>( sop->getParameterised() );
+				result = op->resultParameter()->getValue();
+			}
+			else if ( passData.type() == IECoreHoudini::NodePassData::CORTEX_PROCEDURALHOLDER )
+			{
+				IECore::ParameterisedProcedural *procedural = IECore::runTimeCast<IECore::ParameterisedProcedural>( sop->getParameterised() );
+				IECore::CapturingRendererPtr renderer = new IECore::CapturingRenderer();
+				// We are acquiring and releasing the GIL here to ensure that it is released when we render. This has
+				// to be done because a procedural might jump between c++ and python a few times (i.e. if it spawns
+				// subprocedurals that are implemented in python). In a normal call to cookMySop, this wouldn't be an
+				// issue, but if cookMySop was called from HOM, hou.Node.cook appears to be holding onto the GIL.
+				IECorePython::ScopedGILLock gilLock;
+				{
+					IECorePython::ScopedGILRelease gilRelease;
+					{
+						IECore::WorldBlock worldBlock( renderer );
+						procedural->render( renderer );
+					}
+				}
+				result = IECore::runTimeCast<const IECore::Object>( renderer->world() );
+			}
+			else
+			{
+				continue;
+			}
+			
+			try
+			{
+				inputParameter->setValidatedValue( IECore::constPointerCast<IECore::Object>( result ) );
+			}
+			catch ( const IECore::Exception &e )
+			{
+				addError( SOP_MESSAGE, e.what() );
+			}
+		}
+		else
+		{
+			// looks like a regular Houdini detail
+			IECore::ObjectParameterPtr objectParameter = IECore::runTimeCast<IECore::ObjectParameter>( inputParameter );
+			if ( !objectParameter )
+			{
+				continue;
+			}
+			
+			FromHoudiniGeometryConverterPtr converter = FromHoudiniGeometryConverter::create( inputHandle, objectParameter->validTypes() );
+			if ( !converter )
+			{
+				continue;
+			}
+			
+			// set converter parameters from the node values
+			const CompoundParameter::ParameterVector &converterParameters = converter->parameters()->orderedParameters();
+			for ( CompoundParameter::ParameterVector::const_iterator it=converterParameters.begin(); it != converterParameters.end(); ++it )
+			{
+				updateParameter( *it, now, "parm_" + inputParameter->name() + "_" );
+			}
+			
+			try
+			{
+				IECore::ObjectPtr converted = converter->convert();
+				if ( converted )
+				{
+					inputParameter->setValidatedValue( converted );
+				}
+			}
+			catch ( const IECore::Exception &e )
+			{
+				addError( SOP_MESSAGE, e.what() );
+			}
+			catch ( std::runtime_error &e )
+			{
+				addError( SOP_MESSAGE, e.what() );
+			}
+		}
+	}
+}
---
+++ tags/8.0.0a7/src/IECoreHoudini/ToHoudiniGeometryConverter.cpp
@@ -0,0 +1,422 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2010-2013, Image Engine Design Inc. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+//     * Neither the name of Image Engine Design nor the names of any
+//       other contributors to this software may be used to endorse or
+//       promote products derived from this software without specific prior
+//       written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+//  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+//  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+//  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "UT/UT_StringMMPattern.h"
+
+#include "IECore/CompoundData.h"
+#include "IECore/CompoundParameter.h"
+#include "IECore/MessageHandler.h"
+
+#include "IECoreHoudini/Convert.h"
+#include "IECoreHoudini/ToHoudiniAttribConverter.h"
+#include "IECoreHoudini/ToHoudiniGeometryConverter.h"
+#include "IECoreHoudini/ToHoudiniStringAttribConverter.h"
+
+using namespace IECore;
+using namespace IECoreHoudini;
+
+IE_CORE_DEFINERUNTIMETYPED( ToHoudiniGeometryConverter );
+
+ToHoudiniGeometryConverter::ToHoudiniGeometryConverter( const VisibleRenderable *renderable, const std::string &description )
+	:	ToHoudiniConverter( description, VisibleRenderableTypeId )
+{
+	srcParameter()->setValue( (VisibleRenderable *)renderable );
+	
+	m_convertStandardAttributesParameter = new BoolParameter(
+		"convertStandardAttributes",
+		"Performs automated conversion of standard PrimitiveVariables to Houdini Attributes (i.e. Pref->rest ; Cs->Cd ; s,t->uv)",
+		true
+	);
+	
+	m_attributeFilterParameter = new StringParameter(
+		"attributeFilter",
+		"A list of attribute names to convert, if they exist. Uses Houdini matching syntax.",
+		"*"
+	);
+	
+	parameters()->addParameter( m_convertStandardAttributesParameter );
+	parameters()->addParameter( m_attributeFilterParameter );
+}
+
+ToHoudiniGeometryConverter::~ToHoudiniGeometryConverter()
+{
+}
+
+BoolParameter *ToHoudiniGeometryConverter::convertStandardAttributesParameter()
+{
+	return m_convertStandardAttributesParameter;
+}
+
+const BoolParameter *ToHoudiniGeometryConverter::convertStandardAttributesParameter() const
+{
+	return m_convertStandardAttributesParameter;
+}
+
+StringParameter *ToHoudiniGeometryConverter::attributeFilterParameter()
+{
+	return m_attributeFilterParameter;
+}
+
+const StringParameter *ToHoudiniGeometryConverter::attributeFilterParameter() const
+{
+	return m_attributeFilterParameter;
+}
+
+bool ToHoudiniGeometryConverter::convert( GU_DetailHandle handle ) const
+{
+	ConstCompoundObjectPtr operands = parameters()->getTypedValidatedValue<CompoundObject>();
+	GU_DetailHandleAutoWriteLock writeHandle( handle );
+	
+	GU_Detail *geo = writeHandle.getGdp();
+	if ( !geo )
+	{
+		return false;
+	}
+	
+	const VisibleRenderable *renderable = IECore::runTimeCast<const VisibleRenderable>( srcParameter()->getValidatedValue() );
+	if ( !renderable )
+	{
+		return false;
+	}
+	
+	return doConversion( renderable, geo );
+}
+
+GA_Range ToHoudiniGeometryConverter::appendPoints( GA_Detail *geo, size_t numPoints ) const
+{
+	if ( !numPoints )
+	{
+		return GA_Range();
+	}
+	
+	GA_OffsetList offsets;
+	offsets.reserve( numPoints );
+	
+	for ( size_t i=0; i < numPoints; ++i )
+	{
+		offsets.append( geo->appendPoint() );
+	}
+	
+	return GA_Range( geo->getPointMap(), offsets );
+}
+
+PrimitiveVariable ToHoudiniGeometryConverter::processPrimitiveVariable( const IECore::Primitive *primitive, const PrimitiveVariable &primVar ) const
+{
+	return primVar;
+}
+
+void ToHoudiniGeometryConverter::transferAttribs( GU_Detail *geo, const GA_Range &points, const GA_Range &prims ) const
+{
+	const Primitive *primitive = IECore::runTimeCast<const Primitive>( srcParameter()->getValidatedValue() );
+	if ( primitive )
+	{
+		transferAttribValues( primitive, geo, points, prims );
+	}
+}
+
+void ToHoudiniGeometryConverter::transferAttribValues(
+	const Primitive *primitive, GU_Detail *geo,
+	const GA_Range &points, const GA_Range &prims,
+	PrimitiveVariable::Interpolation vertexInterpolation,
+	PrimitiveVariable::Interpolation primitiveInterpolation,
+	PrimitiveVariable::Interpolation pointInterpolation,
+	PrimitiveVariable::Interpolation detailInterpolation
+) const
+{
+	GA_OffsetList offsets;
+	if ( prims.isValid() )
+	{
+		const GA_PrimitiveList &primitives = geo->getPrimitiveList();
+		for ( GA_Iterator it=prims.begin(); !it.atEnd(); ++it )
+		{
+			const GA_Primitive *prim = primitives.get( it.getOffset() );
+			size_t numPrimVerts = prim->getVertexCount();
+			for ( size_t v=0; v < numPrimVerts; v++ )
+			{
+				if ( prim->getTypeId() == GEO_PRIMPOLY )
+				{
+					offsets.append( prim->getVertexOffset( numPrimVerts - 1 - v ) );
+				}
+				else
+				{
+					offsets.append( prim->getVertexOffset( v ) );
+				}
+			}
+		}
+	}
+
+	GA_Range vertRange( geo->getVertexMap(), offsets );
+	
+	UT_String filter( attributeFilterParameter()->getTypedValue() );
+	
+	// match all the string variables to each associated indices variable
+	/// \todo: replace all this logic with IECore::IndexedData once it exists...
+	PrimitiveVariableMap stringsToIndices;
+	for ( PrimitiveVariableMap::const_iterator it=primitive->variables.begin() ; it != primitive->variables.end(); it++ )
+	{
+		if ( !primitive->isPrimitiveVariableValid( it->second ) )
+		{
+			IECore::msg( IECore::MessageHandler::Warning, "ToHoudiniGeometryConverter", "PrimitiveVariable " + it->first + " is invalid. Ignoring." );
+			filter += UT_String( " ^" + it->first );
+			continue;
+		}
+
+		ToHoudiniAttribConverterPtr converter = ToHoudiniAttribConverter::create( it->second.data );
+		if ( !converter )
+		{
+			continue;
+		}
+		
+		if ( it->second.data->isInstanceOf( StringVectorDataTypeId ) )
+		{
+			std::string indicesVariableName = it->first + "Indices";
+			PrimitiveVariableMap::const_iterator indices = primitive->variables.find( indicesVariableName );
+			if ( indices != primitive->variables.end() && indices->second.data->isInstanceOf( IntVectorDataTypeId ) && primitive->isPrimitiveVariableValid( indices->second ) )
+			{
+				stringsToIndices[it->first] = indices->second;
+				filter += UT_String( " ^" + indicesVariableName );
+			}
+		}
+	}
+	
+	bool convertStandardAttributes = m_convertStandardAttributesParameter->getTypedValue();
+	if ( convertStandardAttributes && UT_String( "s" ).multiMatch( filter ) && UT_String( "t" ).multiMatch( filter ) )
+	{
+		// convert s and t to uv
+		PrimitiveVariableMap::const_iterator sPrimVar = primitive->variables.find( "s" );
+		PrimitiveVariableMap::const_iterator tPrimVar = primitive->variables.find( "t" );
+		if ( sPrimVar != primitive->variables.end() && tPrimVar != primitive->variables.end() )
+		{
+			if ( sPrimVar->second.interpolation == tPrimVar->second.interpolation )
+			{
+				const FloatVectorData *sData = runTimeCast<const FloatVectorData>( sPrimVar->second.data );
+				const FloatVectorData *tData = runTimeCast<const FloatVectorData>( tPrimVar->second.data );
+				if ( sData && tData )
+				{
+					const std::vector<float> &s = sData->readable();
+					const std::vector<float> &t = tData->readable();
+					
+					std::vector<Imath::V3f> uvw;
+					uvw.reserve( s.size() );
+					for ( size_t i=0; i < s.size(); ++i )
+					{
+						uvw.push_back( Imath::V3f( s[i], 1 - t[i], 0 ) );
+					}
+					
+					GA_Range range = vertRange;
+					if ( sPrimVar->second.interpolation == pointInterpolation )
+					{
+						range = points;
+					}
+					
+					ToHoudiniAttribConverterPtr converter = ToHoudiniAttribConverter::create( new V3fVectorData( uvw ) );
+					converter->convert( "uv", geo, range );
+					filter += " ^s ^t";
+				}
+			}
+		}
+	}
+	
+ 	UT_StringMMPattern attribFilter;
+	attribFilter.compile( filter );
+	
+	// add the primitive variables to the various GEO_AttribDicts based on interpolation type
+	for ( PrimitiveVariableMap::const_iterator it=primitive->variables.begin() ; it != primitive->variables.end(); it++ )
+	{
+		UT_String varName( it->first );
+		if ( !varName.multiMatch( attribFilter ) )
+		{
+			continue;
+		}
+		
+		PrimitiveVariable primVar = processPrimitiveVariable( primitive, it->second );
+		ToHoudiniAttribConverterPtr converter = ToHoudiniAttribConverter::create( primVar.data );
+		if ( !converter )
+		{
+			continue;
+		}
+		
+		PrimitiveVariable::Interpolation interpolation = primVar.interpolation;
+		
+		if ( converter->isInstanceOf( (IECore::TypeId)ToHoudiniStringVectorAttribConverterTypeId ) )
+		{
+			PrimitiveVariableMap::const_iterator indices = stringsToIndices.find( it->first );
+			if ( indices != stringsToIndices.end() )
+			{
+				ToHoudiniStringVectorAttribConverter *stringVectorConverter = IECore::runTimeCast<ToHoudiniStringVectorAttribConverter>( converter );
+				PrimitiveVariable indicesPrimVar = processPrimitiveVariable( primitive, indices->second );
+				stringVectorConverter->indicesParameter()->setValidatedValue( indicesPrimVar.data );
+				interpolation = indices->second.interpolation;
+			}
+		}
+		
+		const std::string name = ( convertStandardAttributes ) ? processPrimitiveVariableName( it->first ) : it->first;
+		
+		if ( interpolation == detailInterpolation )
+ 		{
+			// add detail attribs
+			converter->convert( name, geo );
+	 	}
+		else if ( interpolation == pointInterpolation )
+		{
+			// add point attribs
+			if ( name == "P" )
+			{
+				// special case for P
+				transferP( runTimeCast<const V3fVectorData>( primVar.data ), geo, points );
+			}
+			else
+			{
+ 				converter->convert( name, geo, points );
+			}
+		}
+		else if ( interpolation == primitiveInterpolation )
+		{
+			// add primitive attribs
+			converter->convert( name, geo, prims );
+		}
+		else if ( interpolation == vertexInterpolation )
+		{
+			// add vertex attribs
+			converter->convert( name, geo, vertRange );
+		}
+	}
+	
+	// add the name attribute based on blindData
+	const StringData *nameData = primitive->blindData()->member<StringData>( "name" );
+	if ( nameData )
+	{
+		if ( prims.isValid() )
+		{
+			StringVectorDataPtr nameVectorData = new StringVectorData();
+			nameVectorData->writable().push_back( nameData->readable() );
+			std::vector<int> indexValues( prims.getEntries(), 0 );
+			IntVectorDataPtr indexData = new IntVectorData( indexValues );
+			ToHoudiniStringVectorAttribConverterPtr converter = new ToHoudiniStringVectorAttribConverter( nameVectorData );
+			converter->indicesParameter()->setValidatedValue( indexData );
+			converter->convert( "name", geo, prims );
+		}
+	}
+}
+
+void ToHoudiniGeometryConverter::transferP( const IECore::V3fVectorData *positions, GU_Detail *geo, const GA_Range &points ) const
+{
+	if ( !positions )
+	{
+		return;
+	}
+	
+	const std::vector<Imath::V3f> &pos = positions->readable();
+	
+	size_t i = 0;
+	for ( GA_Iterator it=points.begin(); !it.atEnd(); ++it, ++i )
+	{
+		geo->setPos3( it.getOffset(), IECore::convert<UT_Vector3>( pos[i] ) );
+	}
+}
+
+const std::string ToHoudiniGeometryConverter::processPrimitiveVariableName( const std::string &name ) const
+{
+	/// \todo: This should probably be some formal static map. Make sure to update FromHoudiniGeometryConverter as well.
+	if ( name == "Cs" )
+	{
+		return "Cd";
+	}
+	else if ( name == "Os" )
+	{
+		return "Alpha";
+	}
+	else if ( name == "Pref" )
+	{
+		return "rest";
+	}
+	else if ( name == "width" )
+	{
+		return "pscale";
+	}
+	
+	return name;
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+// Factory
+/////////////////////////////////////////////////////////////////////////////////
+
+ToHoudiniGeometryConverterPtr ToHoudiniGeometryConverter::create( const VisibleRenderable *renderable )
+{
+	const TypesToFnsMap *m = typesToFns();
+	TypesToFnsMap::const_iterator it = m->find( Types( renderable->typeId() ) );
+	if( it!=m->end() )
+	{
+		return it->second( renderable );
+	}
+	
+	return 0;
+}
+
+void ToHoudiniGeometryConverter::registerConverter( IECore::TypeId fromType, CreatorFn creator )
+{
+	TypesToFnsMap *m = typesToFns();
+	m->insert( TypesToFnsMap::value_type( Types( fromType ), creator ) );
+}
+
+ToHoudiniGeometryConverter::TypesToFnsMap *ToHoudiniGeometryConverter::typesToFns()
+{
+	static TypesToFnsMap *m = new TypesToFnsMap;
+	return m;
+}
+
+void ToHoudiniGeometryConverter::supportedTypes( std::set<IECore::TypeId> &types )
+{
+	types.clear();
+	
+	const TypesToFnsMap *m = typesToFns();
+	for ( TypesToFnsMap::const_iterator it=m->begin(); it != m->end(); it ++ )
+	{
+		types.insert( it->first.fromType );
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+// Implementation of nested Types class
+/////////////////////////////////////////////////////////////////////////////////
+
+ToHoudiniGeometryConverter::Types::Types( IECore::TypeId from ) : fromType( from )
+{
+}
+
+bool ToHoudiniGeometryConverter::Types::operator < ( const Types &other ) const
+{
+	return fromType < other.fromType;
+}
